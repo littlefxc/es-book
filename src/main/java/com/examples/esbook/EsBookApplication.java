2f35085b78c04c8fff470cdf985e45b96da377c9
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-08-05 20:00:12 +0800

    Initial commit

---
+++ src/main/java/com/examples/esbook/EsBookApplication.java
@@ -0,0 +1,13 @@
+package com.examples.esbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsBookApplication.class, args);
+    }
+
+}
