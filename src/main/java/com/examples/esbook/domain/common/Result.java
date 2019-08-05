12d4afb21c2066de6f07dc3738561404ef63f57b
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-08-05 23:09:16 +0800

    docker 安装中文分词插件

--- src/main/java/com/examples/esbook/domain/common/Result.java
+++ src/main/java/com/examples/esbook/domain/common/Result.java
@@ -1,25 +1,45 @@
 package com.examples.esbook.domain.common;
 
+import lombok.Data;
+
 /**
  * @author fengxuechao
  * @version 0.1
  * @date 2019/8/5
  */
+@Data
 public class Result {
 
+    private Integer code;
+
+    private String msg;
+
+    private Object data;
+
+    public Result(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Result(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
     public static Result error() {
-        return null;
+        return new Result(0, "Failure");
     }
 
     public static Result ok(Object data) {
-        return null;
+        return new Result(200, "Success", data);
     }
 
     public static Result error(String msg) {
-        return null;
+        return new Result(0, msg);
     }
 
     public static Result ok() {
-        return null;
+        return new Result(200, "Success");
     }
 }
